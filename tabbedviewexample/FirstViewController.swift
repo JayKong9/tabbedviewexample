3dbd0a5c87741f0b711f4ba051f173264ccdf1ce
template <342720@npcmg31a-08.local>
2019-11-21 13:47:17 +0000

    kkkkkkk

--- tabbedviewexample/FirstViewController.swift
+++ tabbedviewexample/FirstViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var tories = ["Boris Johnson", "Michael Gove", "Theresa May"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) ->
+    String? {
+        return tories[row]
+        }
 
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        toryLabel.text = tories[row]
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    Conservative.delegate = self
+    Conservative.dataSource = self
     }
 
 
+    @IBOutlet weak var Conservative: UIPickerView!
+    @IBOutlet weak var toryLabel: UILabel!
 }
 
--- tabbedviewexample/SecondViewController.swift
+++ tabbedviewexample/SecondViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var labour = ["Jeremy Corbyn", "Harriet Harman","Ed Miliband"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return labour.count
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return labour[row]
+    }
 
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        labourlabel.text = labour[row]
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    labourParty.delegate = self
+    labourParty.dataSource = self
+        
     }
-
-
+    
+    @IBOutlet weak var labourlabel: UILabel!
+    
+    @IBOutlet weak var labourParty: UIPickerView!
 }
 
